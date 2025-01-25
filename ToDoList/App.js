726511619e40565a5c5e1dd1ed0a81e6e0f05d8c
enismziya <ziyaenismert@gmail.com>
2025-01-25 19:48:54 +0300

    Goal add button and goals show on screen

--- ToDoList/App.js
+++ ToDoList/App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [goalList, setGoalList] = useState([]);
+
+  function goalInputHandler(text) {
+    setEnteredGoalText(text);
+  }
+
+  function addGoalHandler() {
+    if (enteredGoalText.trim().length === 0) return;
+    setGoalList(currentGoalList => [
+      ...currentGoalList,
+      { key: Math.random().toString(), value: enteredGoalText },
+    ]);
+    setEnteredGoalText("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your next goal..."
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+      <FlatList
+        data={goalList}
+        renderItem={(itemData) => (
+          <View style={styles.goalList}>
+            <Text style={styles.goalTexts}>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +43,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 50,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  goalList: {
+    marginVertical: 8,
+    padding: 10,
+    borderRadius: 6,
+    backgroundColor: '#e3e3e3',
+  },
+  goalTexts: {
+    color: 'black',
+    fontSize: 16,
   },
 });
